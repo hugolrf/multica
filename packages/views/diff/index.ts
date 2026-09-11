22ef66ba2bab847eacb528fcc35aa8d49e7d5232
hugolrf <hugo@fourtime.com>
2026-09-10 23:15:17 -0300

    feat(diff): export VscodeOpenMenuItem

--- packages/views/diff/index.ts
+++ packages/views/diff/index.ts
@@ -1,3 +1,4 @@
 export { DiffModal } from "./diff-modal";
 export { DiffClientProvider } from "./diff-context";
 export { DiffMenuItem } from "./diff-menu-item";
+export { VscodeOpenMenuItem } from "./vscode-open-menu-item";
